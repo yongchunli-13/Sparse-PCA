41a48364c54a11def7268547f30f2a5f085db7c8
yongchunli-13 <59891114+yongchunli-13@users.noreply.github.com>
2023-08-12 00:04:17 -0400

    Add files via upload

--- exact algorithms/opt_gen.py
+++ exact algorithms/opt_gen.py
@@ -9,6 +9,7 @@ Created on Sun Aug  6 18:08:42 2023
 
 import MILP
 import Branchandcut
+import Branchandcut_15
 import pandas as pd
 import numpy as np
 import spca_gurobi
@@ -20,62 +21,44 @@ gen_data = MILP.gen_data
 milp22 = MILP.milp22
 
 spca_bc = Branchandcut.spca_bc
+spca_bc_15 = Branchandcut_15.spca_bc
 
-n = 13
+n = 26
 gen_data(n)
 
-# # Optimal values of SPCA using MILP and Branch-and-cut
-# df_Opt = pd.DataFrame(columns=('n', 's', 'MISDP 6', 'MISDP 6 UB', 'MISDPtime', 'MILP 22', 'MILP 22 UB','MILPtime'))
-# loc = 0
-# for s in range(20, 30, 10): # set the values of s
-#     print("This is case", loc+1)
-
-# # #------ MISDP (6) ------
-#     LB_6, UB_6, time_6 = spca_bc(n, s)
-
-# # #------ MILP (22) ------
-#     # LB_22, UB_22, time_22 = milp22(n, s)
-#     # print(LB_22, UB_22, time_22 )
-    
-     
-# # #------ benchmark (17) ------
-    
-#     df_Opt.loc[loc] = np.array([n, s, LB_6, UB_6, time_6, LB_22, UB_22, time_22])
-#     loc = loc+1 
-    
-
-
-# # Optimal values of SPCA using Solver Gorubi
-# n = 64
-# spca = spca_gurobi.spca
-# df_Opt1 = pd.DataFrame(columns=('n', 's', 'SPCA 24', 'SPCA 24 UB', 'SPCAtime'))
-# loc = 0
-# for s in range(10, 30, 10): # set the values of s
-#     print("This is case", loc+1)
-
-# # #------ SPCA (24) ------
-#     LB_24, UB_24, time_24 = spca(n, s)
-
-#     df_Opt1.loc[loc] = np.array([n, s, LB_24, UB_24, time_24])
-#     loc = loc+1 
-    
-
-# Upper bounds of SDP (13)
-spca_rel_thirteen = MILP.spca_rel_thirteen
-n=13
-gen_data(n)
-df_UB = pd.DataFrame(columns=('n', 's', 'LB', 'SDP 13', 'SDP 13 time'))
+# Optimal values of SPCA using MILP and Branch-and-cut
+df_Opt = pd.DataFrame(columns=('n', 's', 'MISDP 6', 'MISDP 6 UB', 'MISDPtime', 'MISDP 15', 'MISDP 15 UB', 'MISDP15time', 'MILP 22', 'MILP 22 UB','MILPtime'))
 loc = 0
-for s in range(4, 11): # set the values of s
+for s in range(5, 10, 10): # set the values of s
     print("This is case", loc+1)
 
-# #------ SPCA (13) ------
-    start = datetime.datetime.now()
-    LB, UB = spca_rel_thirteen(n, s)
-    end = datetime.datetime.now()
-    time = (end-start).seconds
+#------ MISDP (6) ------
+    LB_6, UB_6, time_6 = spca_bc(n, s)
+    
+#------ MISDP (15) ------
+    LB_15, UB_15, time_15 = spca_bc_15(n, s)
 
-    df_UB.loc[loc] = np.array([n, s, LB, (UB-LB)/LB, time])
+#------ MILP (22) ------
+    LB_22, UB_22, time_22 = milp22(n, s)
+    print(LB_22, UB_22, time_22 )
+    
+     
+    df_Opt.loc[loc] = np.array([n, s, LB_6, UB_6, time_6, LB_15, UB_15, time_15, LB_22, UB_22, time_22])
     loc = loc+1 
-   
+    
+
+
+# Optimal values of SPCA using Solver Gorubi
+n = 26
+spca = spca_gurobi.spca
+df_Opt1 = pd.DataFrame(columns=('n', 's', 'SPCA 24', 'SPCA 24 UB', 'SPCAtime'))
+loc = 0
+for s in range(5, 10, 10): # set the values of s
+    print("This is case", loc+1)
+
+# #------ SPCA (24) ------
+    LB_24, UB_24, time_24 = spca(n, s)
+
+    df_Opt1.loc[loc] = np.array([n, s, LB_24, UB_24, time_24])
+    loc = loc+1 
     
